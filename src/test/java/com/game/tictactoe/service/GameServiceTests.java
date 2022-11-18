f000503aed2fca592ae2bc383fafc5b63d9a1339
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-18 18:31:08 +0530

    refactor: Extract service initialization to setup method

--- src/test/java/com/game/tictactoe/service/GameServiceTests.java
+++ src/test/java/com/game/tictactoe/service/GameServiceTests.java
@@ -3,6 +3,7 @@ package com.game.tictactoe.service;
 import com.game.tictactoe.domain.Player;
 import com.game.tictactoe.exception.InvalidTurnException;
 import com.game.tictactoe.util.GameBoard;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -12,11 +13,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class GameServiceTests {
 
-    @Test
-    public void xShouldAlwaysMoveFirst() {
+    private GameService gameService;
+
+    @Before
+    public void setUp() {
 
         GameBoard gameBoard = new GameBoard();
-        GameService gameService = new GameService(gameBoard);
+        gameService = new GameService(gameBoard);
+    }
+
+    @Test
+    public void xShouldAlwaysMoveFirst() {
 
         assertThat(gameService.playGame(Player.X, 0, 1)).isEqualTo("Successful Move");
     }
@@ -24,17 +31,11 @@ public class GameServiceTests {
     @Test(expected = InvalidTurnException.class)
     public void isPlayerOMovingFirst() {
 
-        GameBoard gameBoard = new GameBoard();
-        GameService gameService = new GameService(gameBoard);
-
         gameService.playGame(Player.O, 0, 0);
     }
 
     @Test
     public void savePositionOnBoard() {
-
-        GameBoard gameBoard = new GameBoard();
-        GameService gameService = new GameService(gameBoard);
 
         assertThat(gameService.playGame(Player.X, 0, 2)).isEqualTo("Successful Move");
     }
