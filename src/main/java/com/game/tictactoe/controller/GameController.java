24c9428adc6829cba19b8c619def60b1f46db058
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-18 21:25:37 +0530

    build: Add swagger

--- src/main/java/com/game/tictactoe/controller/GameController.java
+++ src/main/java/com/game/tictactoe/controller/GameController.java
@@ -1,7 +1,13 @@
 package com.game.tictactoe.controller;
 
+import com.game.tictactoe.domain.GameErrorResponse;
 import com.game.tictactoe.domain.Player;
 import com.game.tictactoe.service.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +23,14 @@ public class GameController {
         this.gameService = gameService;
     }
 
+    @Operation(summary = "Play Tic Tac Toe Game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully played the Turn",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "409", description = "Invalid Position parameter",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GameErrorResponse.class))})})
     @PostMapping(value = "/tic-tac-toe/play/{player}/{position}")
     public ResponseEntity<String> PlayGameHandler(@PathVariable(name = "player") Player player,
                                                   @PathVariable(name = "position") int position) {
